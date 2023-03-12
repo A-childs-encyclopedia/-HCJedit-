a044d9e993dbcaa3a5663c051d2abb872b31c3b4
A-childs-encyclopedia <84061672+A-childs-encyclopedia@users.noreply.github.com>
2023-03-12 13:40:54 +0530

    Update HelpCommandHandlers.js

--- src/help/HelpCommandHandlers.js
+++ src/help/HelpCommandHandlers.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 - present Cherrycream co.
+ * Copyright (c) 2012 - present Cherrycream co. All rights reserved
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
